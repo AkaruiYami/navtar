d8162989fcb3017dfe7dc1e7b794a37d5b387ea7
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2025-06-23 00:48:35 +0800

    retrieving the data from local save file

--- src/commands.rs
+++ src/commands.rs
@@ -1,5 +1,14 @@
 use clap::{Parser, Subcommand};
 
+/// A CLI tool to manage workspace directory powered by Rust.
+///
+/// This tool used to quickly navigate to your workspace directory
+/// without the need to put lengthy path when trying to cd into it.
+/// By just using using the unique workspace ID, we can cd into it.
+/// The ID can be reistered using the Add command. However, if you try
+/// to navigate into a workspace using ID that is not registered, it
+/// will assume that you are trying to registered the current directory
+/// into the managegment system using the given unregistered ID.
 #[derive(Parser)]
 #[command(name = "wsp", about = "CLI workspace directory mangement.")]
 pub struct Cli {
@@ -7,10 +16,31 @@ pub struct Cli {
     pub cmd: Option<Command>,
 }
 
+/// Define the operation can be execute upon the workspace directory manaement system.
+///
+/// This include, adding, gettin, removing, and listing the workspace.
 #[derive(Subcommand)]
 pub enum Command {
-    Add { name: String, path: String },
-    Get { name: String },
-    Remove { name: String },
+    /// Register the directory with the given name as the unique ID.
+    Add {
+        /// The unique ID for the directory.
+        name: String,
+        /// The path to the directory.
+        path: String,
+    },
+
+    /// Fetch the path based on the registered unique ID.
+    Get {
+        /// The registered unique ID of the directory.
+        name: String,
+    },
+
+    /// Remove the path based on the registered unique ID.
+    Remove {
+        /// The registered unique ID of the directory.
+        name: String,
+    },
+
+    /// List all registered ID together with its associated path.
     List,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,84 @@
 #![allow(unused)]
 
-use clap::Parser;
+use std::{
+    collections::HashMap,
+    env,
+    fs::{self, File},
+    io::{self, BufRead, BufReader},
+    path::{Path, PathBuf},
+};
+
+use clap::{CommandFactory, Parser};
 mod commands;
+
+struct Workspace {
+    name: String,
+    path: PathBuf,
+}
+
+impl Workspace {
+    fn new(name: &str, path: &str) -> Workspace {
+        let mut path_buf = PathBuf::new();
+        path_buf.push(path);
+        Workspace {
+            name: name.to_string(),
+            path: path_buf,
+        }
+    }
+
+    fn exists(&self) -> bool {
+        self.path.exists()
+    }
+
+    fn is_dir(&self) -> bool {
+        self.path.is_dir()
+    }
+
+    fn is_file(&self) -> bool {
+        self.path.is_file()
+    }
+}
+
+fn get_storage_path() -> PathBuf {
+    PathBuf::from(env::var("NAVTAR_DIR").expect("Please set NAVTAR_DIR in environment variable."))
+}
+
+fn load_workspaces() -> Vec<Workspace> {
+    let root_path = get_storage_path();
+
+    // TODO: Handle if the file is not there (create default)
+    let file = File::open(root_path.join(".data")).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut result: Vec<Workspace> = Vec::new();
+    for line in reader.lines().map_while(Result::ok) {
+        if let Some((name, path_string)) = line.split_once("-->") {
+            let ws = Workspace::new(name.trim(), path_string.trim());
+            result.push(ws);
+        }
+    }
+    result
+}
 
 fn main() {
     let parser = commands::Cli::parse();
-    todo!("Handle each command");
+
+    let all_workspace = load_workspaces();
     match parser.cmd {
-        Some(commands::Command::Add { name, path }) => {}
-        Some(commands::Command::Get { name }) => {}
-        Some(commands::Command::Remove { name }) => {}
-        Some(commands::Command::List) => {}
-        None => {}
+        Some(commands::Command::Add { name, path }) => {
+            todo!() // TODO: Implement registering new workspace
+        }
+        Some(commands::Command::Get { name }) => {
+            todo!() // TODO: Implement the retrieval of workspace path
+        }
+        Some(commands::Command::Remove { name }) => {
+            todo!() // TODO: Implement the removing of the exisitng workspace
+        }
+        Some(commands::Command::List) => {
+            for (i, ws) in all_workspace.iter().enumerate() {
+                println!("[{}] {} = {}", i + 1, ws.name, ws.path.to_str().unwrap());
+            }
+        }
+        None => commands::Cli::command().print_help().unwrap(),
     }
 }
