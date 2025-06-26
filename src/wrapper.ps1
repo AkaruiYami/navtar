59c4d160c1c189b831f104864332f0a9da806869
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2025-06-27 03:19:36 +0800

    fix wrapper to properly print help especially for each command

--- src/wrapper.ps1
+++ src/wrapper.ps1
@@ -21,34 +21,48 @@ function wm {
 
     switch ($Args[0]) {
         "list" {
+            if ($Args.Count -eq 2 -and $Args[1] -eq "-h") {
+                & $navtarExe list -h
+            }
             & $navtarExe list
         }
         "add" {
             if ($Args.Count -lt 3) {
-                Write-Host "Usage: wm add <name> <path>" -ForegroundColor Yellow
+                # Write-Host "Usage: wm add <name> <path>" -ForegroundColor Yellow
+                & $navtarExe add -h
             } else {
                 & $navtarExe add $Args[1] $Args[2]
             }
         }
         "remove" {
             if ($Args.Count -lt 2) {
-                Write-Host "Usage: wm remove <name>" -ForegroundColor Yellow
+                # Write-Host "Usage: wm remove <name>" -ForegroundColor Yellow
+                & $navtarExe remove -h
             } else {
                 & $navtarExe remove $Args[1]
             }
         }
         "get" {
             if ($Args.Count -lt 2) {
-                Write-Host "Usage: wm get <name>" -ForegroundColor Yellow
+                # Write-Host "Usage: wm get <name>" -ForegroundColor Yellow
+                & $navtarExe get -h
             } else {
                 & $navtarExe get $Args[1]
             }
         }
         "rename" {
             if ($Args.Count -lt 3) {
-                Write-Host "Usage: wm rename <old_name> <new_name>" -ForegroundColor Yellow
+                # Write-Host "Usage: wm rename <old_name> <new_name>" -ForegroundColor Yellow
+                & $navtarExe rename -h
             } else {
                 & $navtarExe rename $Args[1] $Args[2]
+            }
+        }
+        {$_ -eq "help" -or $_ -eq "-h" -or $_ -eq "--help"} {
+            if ($Args.Count -eq 2) {
+                & $navtarExe $Args[1] -h
+            } else {
+                & $navtarExe -h
             }
         }
         default {
